3c2a635fc9f62bcf9aa60444c51a374e2bcf5be6
Danka-Andras <danka.andras.balint@students.jedlik.eu>
2024-05-29 12:40:51 +0200

    game added

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ class Login {
 }
 
 const user1 = new Login("", "");
+const Loadingimage = document.getElementById("loadingimage");
+const Loadingimage2 = document.getElementById("loadingimage2");
+
 
 function submitForm() {
     const username1 = document.getElementById('username').value;
     const password1 = document.getElementById('password').value;
-    const Loadingimage = document.getElementById("loadingimage");
     Loadingimage.style.display = "block";
     user1.name = username1;
     user1.password = password1;
@@ -26,9 +28,66 @@ function submitForm() {
 
 function submitForm2() {
     if (user1.name === document.getElementById('lusername').value && user1.password === document.getElementById('lpassword').value) {
-        console.log("Sikeres bejelentkezés");
+        Loadingimage2.style.display = "block";
+
+        setTimeout(() => {
+            alert("Sikeres bejelentkezés")
+            loginn.style.display = "none";
+            game.style.display = "block"
+            Loadingimage2.style.display = "none"; 
+        }, 3000);
     } else {
-        console.log("Nem jó");
+        alert("Hibás felhasználónév vagy jelszó!");
     }
 }
 
+
+var clickCount = 0;
+var clicksPerSecond = 0;
+var counter = document.getElementById('counter');
+var clickArea = document.getElementById('clickArea');
+var timerDisplay = document.getElementById('timer');
+var clicksPerSecondDisplay = document.getElementById('clicksPerSecond');
+var timeLimitInput = document.getElementById('timeLimit');
+var timeLimit = 10; 
+
+function startGame() {
+  clickCount = 0;
+  clicksPerSecond = 0;
+  counter.textContent = 'Kattintások száma: 0';
+  clicksPerSecondDisplay.textContent = 'Másodpercenkénti kattintások: 0';
+  clickArea.style.display = 'block';
+  timeLimit = parseInt(timeLimitInput.value);
+  timerDisplay.textContent = 'Idő: ' + timeLimit;
+  startTimer();
+  startClicksPerSecondCounter();
+}
+
+function startTimer() {
+  var timer = setInterval(function() {
+    timeLimit--;
+    timerDisplay.textContent = 'Idő: ' + timeLimit;
+    if (timeLimit <= 0) {
+      clearInterval(timer);
+      endGame();
+    }
+  }, 1000);
+}
+
+function startClicksPerSecondCounter() {
+  setInterval(function() {
+    clicksPerSecondDisplay.textContent = 'Másodpercenkénti kattintások: ' + clicksPerSecond;
+    clicksPerSecond = 0;
+  }, 1000);
+}
+
+clickArea.addEventListener('click', function() {
+  clickCount++;
+  counter.textContent = 'Kattintások száma: ' + clickCount;
+  clicksPerSecond++;
+});
+
+function endGame() {
+  clickArea.style.display = 'none';
+  alert('A játék véget ért! Összpontszám: ' + clickCount);
+}
